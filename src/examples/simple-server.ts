7f32ff65fa49958043cc6b14cedf226dc28cee56
Ali Sabzevari <alisabzevari@gmail.com>
2019-07-13 13:28:26 +0200

    Move examples to example folder

---
+++ src/examples/simple-server.ts
@@ -0,0 +1,13 @@
+import { HttpRequest, HttpResponse } from "../http";
+import { createServer } from "../http4ts";
+import { Node } from "../node/node";
+
+async function handler(req: HttpRequest): Promise<HttpResponse> {
+  return {
+    body: req.url,
+    headers: {},
+    status: 200
+  };
+}
+
+createServer(handler, new Node(8080)).start();
