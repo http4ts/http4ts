6878d576e892bae558ea4b88ac668050c2b72d3e
Ali Sabzevari <alisabzevari@gmail.com>
2019-07-31 18:39:43 +0200

    Add promise in return type of filter

--- src/http4ts.ts
+++ src/http4ts.ts
@@ -4,7 +4,9 @@ export type HttpHandler = (
   req: HttpRequest
 ) => HttpResponse | Promise<HttpResponse>;
 
-export type HttpFilter = (handler: HttpHandler) => Promise<HttpHandler>;
+export type HttpFilter = (
+  handler: HttpHandler
+) => Promise<HttpHandler> | HttpHandler;
 
 export interface HttpServer {
   start(): Promise<void>;
