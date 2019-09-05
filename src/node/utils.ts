75e2baa8c888e7b0275d18cef3a91f7cca2e56a8
Burim Shala <shalaster@gmail.com>
2019-09-05 15:13:26 +0200

    Implement toStream properly

--- src/node/utils.ts
+++ src/node/utils.ts
@@ -2,12 +2,25 @@ import { Readable } from "stream";
 import { ReadableStream } from "web-streams-polyfill/ponyfill/es2018"
 
 export function toReadableStream(stream: Readable): ReadableStream {
+  let streamClosedAlready = false;
   return new ReadableStream({
     start(controller: ReadableStreamDefaultController) {
-      stream.on("data", chunk => controller.enqueue(chunk));
+      stream.on("data", chunk => {
+        controller.enqueue(chunk);
+      });
       stream.on("error", error => controller.error(error));
-      stream.on("end", () => controller.close());
-      stream.on("close", () => { }); //BSH: Handle close here. Close event is firing after end event causing the ReadableStream to throw errors
+      stream.on("end", () => {
+        if (!streamClosedAlready) {
+          controller.close()
+          streamClosedAlready = true;
+        }
+      });
+      stream.on("close", () => {
+        if (!streamClosedAlready) {
+          controller.close()
+          streamClosedAlready = true;
+        }
+      });
     },
     cancel(reason) {
       stream.destroy(reason)
