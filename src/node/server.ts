92f9bda6b43dfe504f9f5dee0283465e3bb10dec
Ali Sabzevari <alisabzevari@gmail.com>
2019-10-22 19:32:53 +0200

    #24 Simplify node server

---
+++ src/node/server.ts
@@ -0,0 +1,51 @@
+import { RequestListener, IncomingMessage, ServerResponse } from "http";
+
+import { HttpHandler } from "../http4ts";
+import { HttpRequest, HttpResponse } from "../http";
+import { streamToString } from "./utils";
+
+async function translateRequest(
+  nodeReq: IncomingMessage
+): Promise<HttpRequest> {
+  return {
+    body: await streamToString(nodeReq),
+    headers: nodeReq.headers,
+    method: nodeReq.method || "",
+    url: nodeReq.url || ""
+  };
+}
+
+function writeResponse(
+  res: HttpResponse,
+  nodeRes: ServerResponse
+): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+    nodeRes.statusCode = res.status;
+    for (const key in res.headers) {
+      if (res.headers.hasOwnProperty(key)) {
+        const value = res.headers[key];
+        nodeRes.setHeader(key, value || "");
+      }
+    }
+    nodeRes.write(res.body, reject);
+    nodeRes.end();
+    resolve();
+  });
+}
+
+function writeErrorResponse(nodeRes: ServerResponse): void {
+  nodeRes.statusCode = 500;
+  nodeRes.end(); // TODO: what to do with the callback here?
+}
+
+export function toNodeRequestListener(handler: HttpHandler): RequestListener {
+  return async (req, res) => {
+    try {
+      const http4tsResponse = await handler(await translateRequest(req));
+      await writeResponse(http4tsResponse, res);
+    } catch (error) {
+      console.log(error); // TODO: add proper logging or send the error to an error callback
+      writeErrorResponse(res);
+    }
+  };
+}
