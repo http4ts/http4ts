cb649af11b25a72f1065c39a2d7b8e2454b44342
Ali Sabzevari <alisabzevari@gmail.com>
2019-10-30 14:57:20 +0100

    #24 Simplify node server (#25)

--- src/http4ts.ts
+++ src/http4ts.ts
@@ -7,20 +7,3 @@ export type HttpHandler = (
 export type HttpFilter = (
   handler: HttpHandler
 ) => Promise<HttpHandler> | HttpHandler;
-
-export interface HttpServer {
-  start(): Promise<void>;
-  stop(): Promise<void>;
-}
-
-export interface ServerConfig {
-  port: number;
-  toServer(httpHandler: HttpHandler): HttpServer;
-}
-
-export function createServer(
-  httpHandler: HttpHandler,
-  serverConfig: ServerConfig
-): HttpServer {
-  return serverConfig.toServer(httpHandler);
-}
