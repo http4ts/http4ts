63ffb635deed57ccee2c9d3faee07109dbdf8586
Ali Sabzevari <alisabzevari@gmail.com>
2020-04-04 15:20:49 +0200

    Add test for string encoding utils for supporting string iterators

--- src/core/http-body/__tests__/string-encoding-utils.spec.ts
+++ src/core/http-body/__tests__/string-encoding-utils.spec.ts
@@ -1,4 +1,8 @@
+import { TextEncoder } from "util";
+
 import { stringToIterable, iterableToString } from "../string-encoding-utils";
+
+const encoder = new TextEncoder();
 
 describe("string-iterable conversions", () => {
   it("should correctly convert string to iterable and vice versa", async () => {
@@ -6,5 +10,27 @@ describe("string-iterable conversions", () => {
     const it = stringToIterable(text);
 
     expect(await iterableToString(it)).toEqual(text);
+  });
+});
+
+describe("iterableToString", () => {
+  it("should correctly convert string iterables", async () => {
+    async function* gen() {
+      yield "Hello ";
+      yield "world ";
+      yield "😌";
+    }
+
+    expect(await iterableToString(gen())).toBe("Hello world 😌");
+  });
+
+  it("should correctly convert Uint8Array iterables", async () => {
+    async function* gen() {
+      yield encoder.encode("Hello ");
+      yield encoder.encode("world ");
+      yield encoder.encode("😌");
+    }
+
+    expect(await iterableToString(gen())).toBe("Hello world 😌");
   });
 });
