4341d6fec75cecc86dadd22112d8c98a18e51131
Ali Sabzevari <alisabzevari@gmail.com>
2020-04-28 18:55:37 +0200

    #67 Add test utils for Deno

--- src/deno/imports.ts
+++ src/deno/imports.ts
@@ -1,4 +1,7 @@
 export {
   listenAndServe,
-  ServerRequest
+  ServerRequest,
+  Server,
+  serve
 } from "https://deno.land/std@v0.41.0/http/server.ts";
+export * as asserts from "https://deno.land/std@v0.41.0/testing/asserts.ts";
