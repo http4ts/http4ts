cc1ae9fef920b5af655bf575b724c86c9627ea99
Ali Sabzevari <alisabzevari@gmail.com>
2020-05-11 19:41:25 +0200

    #67 Add deno module entry point

---
+++ src/deno/mod.ts
@@ -0,0 +1,7 @@
+import { setupEnvironment } from "../core/env.ts";
+
+setupEnvironment(TextDecoder, TextEncoder);
+
+export * from "../core/mod.ts";
+export * from "./server.ts";
+export * from "./client.ts";
