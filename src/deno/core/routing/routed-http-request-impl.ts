811786815389c5fa8606d3f0827d91d36188e8d8
Ali Sabzevari <alisabzevari@gmail.com>
2020-06-21 16:05:25 +0200

    #56 Fix prototype issue of routed request in router

---
+++ src/deno/core/routing/routed-http-request-impl.ts
@@ -0,0 +1,17 @@
+import { HttpRequestImpl } from "../http-request/http-request-impl.ts";
+import { RoutedHttpRequest } from "./routes.ts";
+import { HttpMethod, HttpBody } from "../http.ts";
+import { RequestHttpHeaders } from "../http-headers.ts";
+
+export class RoutedHttpRequestImpl extends HttpRequestImpl
+  implements RoutedHttpRequest {
+  constructor(
+    public readonly url: string,
+    public readonly method: HttpMethod,
+    public readonly body: HttpBody,
+    public readonly headers: RequestHttpHeaders,
+    public readonly routeParams: Record<string, string>
+  ) {
+    super(url, method, body, headers);
+  }
+}
