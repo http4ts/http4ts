f12a1c623faf3968f8658ab8b48fff70cedbc28e
Ali Sabzevari <alisabzevari@gmail.com>
2021-08-31 21:29:43 +0200

    Use new Deno http server

--- src/deno/examples/example.ts
+++ src/deno/examples/example.ts
@@ -1,20 +1,30 @@
 import { res, HttpStatus, stringBody, HttpRequest } from "../core/mod.ts";
-import { toDenoRequestListener } from "../server.ts";
-import { listenAndServe } from "../deps.ts";
+import { handleDenoRequest } from "../server.ts";
+
+async function handler(req: HttpRequest) {
+  const b = await req.body.asString();
+  console.log(b);
+  return res({
+    status: HttpStatus.LOCKED,
+    headers: { "accept-language": "de-DE" },
+    body: stringBody("Hello world! 😌" + b)
+  });
+};
+
+async function serve(conn: Deno.Conn) {
+  const requests = Deno.serveHttp(conn);
+  for await (const req of requests) {
+    await handleDenoRequest(handler, req);
+  }
+}
 
 async function main() {
-  const handler = async (req: HttpRequest) => {
-    const b = await req.body.asString();
-    console.log(b);
-    return res({
-      status: HttpStatus.LOCKED,
-      headers: { "accept-language": "de-DE" },
-      body: stringBody("Hello world! 😌" + b)
-    });
-  };
-
   console.log("Listening on http://localhost:8000");
-  await listenAndServe({ port: 8000 }, toDenoRequestListener(handler));
+
+  const listener = Deno.listen({ port: 8000 });
+  for await (const conn of listener) {
+    serve(conn)
+  }
 }
 
 await main();
--- src/deno/utils.ts
+++ src/deno/utils.ts
@@ -6,26 +6,28 @@ export function toHttp4tsHeader(fetchHeaders: Headers): HttpHeaders {
   return entries.reduce((acc, [key, val]) => ({ ...acc, [key]: val }), {});
 }
 
-const encoder = new TextEncoder();
-
-export function iterableToReadableStream(
-  iterable: AsyncIterable<Uint8Array | string>
-) {
-  const it = iterable[Symbol.asyncIterator]();
-  return {
-    async read(p: Uint8Array) {
-      const res = await it.next();
-      if (res.done) {
-        return null;
-      } else {
-        const v = res.value;
-        if (typeof v === "string") {
-          p.set(encoder.encode(v));
-        } else {
-          p.set(v);
-        }
-        return res.value.length;
-      }
+export function toHeaders(http4tsHeaders: HttpHeaders): HeadersInit {
+  return Object.entries(http4tsHeaders).map(h => {
+    let value: string[];
+    if (Array.isArray(h[1])) {
+      value = h[1]
+    } else {
+      value = [h[1] ?? ""]
     }
-  };
+    return [h[0], ...value];
+  })
+}
+
+export function iteratorToStream(iterator: AsyncIterator<Uint8Array | string>): ReadableStream<any> {
+  return new ReadableStream({
+    async pull(controller) {
+      const { value, done } = await iterator.next();
+
+      if (done) {
+        controller.close();
+      } else {
+        controller.enqueue(value);
+      }
+    },
+  });
 }
